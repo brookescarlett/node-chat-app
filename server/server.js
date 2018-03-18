51762ecdd5da725c7e37e6116c4f79ef7bb88dd6
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-03-18 13:26:44 -0400

    create generate message utils

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
+const {generateMessage} = require('./utils/message')
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 const app = express()
@@ -11,23 +12,16 @@ const io = socketIO(server)
 
 app.use(express.static(publicPath))
 
-//register an event listener
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // socket.emit('newMessage', {
-  //   from: 'mike@example.com',
-  //   text: 'Hey. What is going on?',
-  //   createdAt: 123
-  // });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    })
+    io.emit('newMessage', generateMessage(message.from, message.text))
   });
 
   socket.on('disconnect', () => {
