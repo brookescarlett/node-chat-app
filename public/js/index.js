649ba1c5a7aa6856be674dbeda9b8459025ce467
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-03-18 14:20:50 -0400

    locatin testing

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function(){
   console.log('Connected to server')
-
-  // socket.emit('createMessage', {
-  //   to: 'address@example.com',
-  //   text: 'hey this is brooke'
-  // });
 });
 
 socket.on('disconnect', function(){
@@ -15,4 +10,45 @@ socket.on('disconnect', function(){
 
 socket.on('newMessage', function(message){
   console.log('newMessage', message)
+  const li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`)
+
+  jQuery('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function (message) {
+  const li = jQuery('<li></li>');
+  const a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  })
+});
+
+const locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Unable to fetch location.');
+  });
 });
--- server/server.js
+++ server/server.js
@@ -19,9 +19,14 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text))
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server');
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
