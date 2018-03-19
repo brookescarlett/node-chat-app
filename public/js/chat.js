36eb1890df9b7d3050448584d11f3b0047698d3c
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-03-19 17:05:14 -0400

    Scroll feature fixed

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,19 +1,8 @@
 const socket = io();
 
 function scrollToBottom () {
-  // Selectors
-  var messages = jQuery('#messages');
-  var newMessage = messages.children('li:last-child')
-  // Heights
-  var clientHeight = messages.prop('clientHeight');
-  var scrollTop = messages.prop('scrollTop');
-  var scrollHeight = messages.prop('scrollHeight');
-  var newMessageHeight = newMessage.innerHeight();
-  var lastMessageHeight = newMessage.prev().innerHeight();
-
-  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
-    messages.scrollTop(scrollHeight);
-  }
+  const element = document.getElementById('messages');
+  element.scrollIntoView({behavior: "instant", block: "end", inline: "nearest"});
 }
 
 socket.on('connect', function(){
